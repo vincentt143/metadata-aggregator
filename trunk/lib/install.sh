dc532f0d4c5243fc3ddab3d248eb1f5370215e3b
intersect.engineering.team@gmail.com <intersect.engineering.team@gmail.com@5d93d96e-e42d-bcce-02ae-4798293442ab>
2013-02-07 07:04:35 +0000

    Add cuke4duke dependency that got removed from main repo

--- trunk/lib/install.sh
+++ trunk/lib/install.sh
@@ -1,2 +1,3 @@
 mvn install:install-file -DgroupId=org.springframework.webflow -DartifactId=org.springframework.webflow -Dversion=2.3.0.RELEASE -Dpackaging=jar -Dfile=org.springframework.webflow-2.3.0.RELEASE.jar
 mvn install:install-file -DgroupId=org.springframework.webflow -DartifactId=org.springframework.js -Dversion=2.3.0.RELEASE -Dpackaging=jar -Dfile=org.springframework.js-2.3.0.RELEASE.jar
+mvn install:install-file -DgroupId=cuke4duke -DartifactId=cuke4duke -Dversion=0.4.3 -Dpackaging=jar -Dfile=cuke4duke-0.4.3.jar
