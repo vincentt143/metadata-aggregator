3ed6fc6c14a05df4394065936d431fa782ee92dc
intersect.engineering.team@gmail.com <intersect.engineering.team@gmail.com@5d93d96e-e42d-bcce-02ae-4798293442ab>
2013-02-27 13:17:05 +0000

    adding a local-dc2d directory containing an env script which has the profile set to dc2d

---
+++ sydma-install/config/local-dc2d/env.sh
@@ -0,0 +1,9 @@
+echo "USING SPECIFIC WAR NAME FOR DC2D (sydma-web.war)"
+JAVA_HOME=/usr/java/jdk1.6.0_39
+TOMCAT6_HOME=/usr/share/tomcat6
+WAR_FILE_REMOTE="sydma-web.war"
+WEBAPP_DIR="sydma-web"
+PROFILE=dc2d
+REMOTE_HOST=localhost
+REMOTE_USER=devel
+DB_ROOT_USER=root
